0da354af1cba1b6632db2bca5d6e47951434cd6a
Teresa Scavotto <jamesscavotto.teresa@titans.easternflorida.edu>
2024-03-14 09:57:39 -0400

    Create DEBUG06_01.cpp

---
+++ Debugging Exercises/DEBUG06_01.cpp
@@ -0,0 +1,33 @@
+// A high school is holding a recycling competition,
+// and this program allows a user to enter a student's 
+// year in school (1 through 4) and number of cans collected
+// for recycling. Data is entered continually until the user
+// enters 9 for the year.
+// After headings, output is four lines --
+// one for each school year class.
+start
+   Declarations
+      num year
+      num cans
+      num SIZE = 4
+      num QUIT = 9
+      num collectedArray[SIZE] = 0, 0, 0
+      string HEAD1 = "Can Recycling Report"
+      string HEAD2 = "Year      Cans Collected"
+   output "Enter year of student or ", QUIT, " to quit "
+   input year
+   while year <> QUIT
+      output "Enter number of cans collected "
+      input cans
+      collectedArray[year] = collectedArray[year] + cans
+      output "Enter year of student or ", QUIT, " to quit "
+      input year
+   endwhile
+   output HEAD1
+   output HEAD2
+   year = 1
+   while year < SIZE
+      output year, collectedArray[year]
+      year = year + 1
+   endwhile
+stop         
